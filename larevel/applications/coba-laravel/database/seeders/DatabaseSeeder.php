7b30c35a9e8692b4143bba933744ffff31458b43
SMKN1-PPLG <smknpplg@gmail.com>
2022-09-17 12:09:29 +0900

    Faker

--- larevel/applications/coba-laravel/database/seeders/DatabaseSeeder.php
+++ larevel/applications/coba-laravel/database/seeders/DatabaseSeeder.php
@@ -17,24 +17,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        User::create([
-            "name" => "Otong",
-            "email" => "otong@gmail.com",
-            "password" => bcrypt('12345')
-        ]);
+        // User::create([
+        //     "name" => "Otong",
+        //     "email" => "otong@gmail.com",
+        //     "password" => bcrypt('12345')
+        // ]);
 
-        User::create([
-            "name" => "Ucup",
-            "email" => "ucup@gmail.com",
-            "password" => bcrypt('12345')
-        ]);
+        // User::create([
+        //     "name" => "Ucup",
+        //     "email" => "ucup@gmail.com",
+        //     "password" => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             "name" => "Makanan",
@@ -46,31 +47,33 @@ class DatabaseSeeder extends Seeder
             "slug" => "pemrograman"
         ]);
 
-        Post::create([
-            "title" => "Membuat Jalangkote",
-            "slug" => "membuat-jalangkote",
-            "excerpt" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti",
-            "body" => " <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti repellat dolorem hic nostrum voluptas quam. Eaque, voluptatem quos explicabo non illum exercitationem placeat. Adipisci earum voluptates eos accusantium quae magni, sint alias deserunt dignissimos.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere obcaecati nulla dolores dolore expedita ullam porro ab enim, fugiat reiciendis unde ipsa doloribus illo! Praesentium magnam ipsa veritatis placeat eveniet!</p>",
-            "category_id" => 1,
-            "user_id" => 1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            "title" => "Membuat Onde-onde",
-            "slug" => "membuat-onde-onde",
-            "excerpt" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti",
-            "body" => " <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti repellat dolorem hic nostrum voluptas quam. Eaque, voluptatem quos explicabo non illum exercitationem placeat. Adipisci earum voluptates eos accusantium quae magni, sint alias deserunt dignissimos.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere obcaecati nulla dolores dolore expedita ullam porro ab enim, fugiat reiciendis unde ipsa doloribus illo! Praesentium magnam ipsa veritatis placeat eveniet!</p>",
-            "category_id" => 1,
-            "user_id" => 1
-        ]);
+        // Post::create([
+        //     "title" => "Membuat Jalangkote",
+        //     "slug" => "membuat-jalangkote",
+        //     "excerpt" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti",
+        //     "body" => " <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti repellat dolorem hic nostrum voluptas quam. Eaque, voluptatem quos explicabo non illum exercitationem placeat. Adipisci earum voluptates eos accusantium quae magni, sint alias deserunt dignissimos.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere obcaecati nulla dolores dolore expedita ullam porro ab enim, fugiat reiciendis unde ipsa doloribus illo! Praesentium magnam ipsa veritatis placeat eveniet!</p>",
+        //     "category_id" => 1,
+        //     "user_id" => 1
+        // ]);
 
-        Post::create([
-            "title" => "Pemrograman Dasar Dengan C#",
-            "slug" => "pemrograman-dasar-dengan-c#",
-            "excerpt" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti",
-            "body" => " <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti repellat dolorem hic nostrum voluptas quam. Eaque, voluptatem quos explicabo non illum exercitationem placeat. Adipisci earum voluptates eos accusantium quae magni, sint alias deserunt dignissimos.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere obcaecati nulla dolores dolore expedita ullam porro ab enim, fugiat reiciendis unde ipsa doloribus illo! Praesentium magnam ipsa veritatis placeat eveniet!</p>",
-            "category_id" => 2,
-            "user_id" => 2
-        ]);
+        // Post::create([
+        //     "title" => "Membuat Onde-onde",
+        //     "slug" => "membuat-onde-onde",
+        //     "excerpt" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti",
+        //     "body" => " <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti repellat dolorem hic nostrum voluptas quam. Eaque, voluptatem quos explicabo non illum exercitationem placeat. Adipisci earum voluptates eos accusantium quae magni, sint alias deserunt dignissimos.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere obcaecati nulla dolores dolore expedita ullam porro ab enim, fugiat reiciendis unde ipsa doloribus illo! Praesentium magnam ipsa veritatis placeat eveniet!</p>",
+        //     "category_id" => 1,
+        //     "user_id" => 1
+        // ]);
+
+        // Post::create([
+        //     "title" => "Pemrograman Dasar Dengan C#",
+        //     "slug" => "pemrograman-dasar-dengan-c#",
+        //     "excerpt" => "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti",
+        //     "body" => " <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, quas nulla atque, culpa minima aspernatur nesciunt laudantium ad quibusdam id adipisci dignissimos laboriosam nisi deleniti repellat dolorem hic nostrum voluptas quam. Eaque, voluptatem quos explicabo non illum exercitationem placeat. Adipisci earum voluptates eos accusantium quae magni, sint alias deserunt dignissimos.</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere obcaecati nulla dolores dolore expedita ullam porro ab enim, fugiat reiciendis unde ipsa doloribus illo! Praesentium magnam ipsa veritatis placeat eveniet!</p>",
+        //     "category_id" => 2,
+        //     "user_id" => 2
+        // ]);
     }
 }
--- larevel/applications/coba-laravel/resources/views/post.blade.php
+++ larevel/applications/coba-laravel/resources/views/post.blade.php
@@ -4,7 +4,7 @@
   <article>
     <h2>{{ $post->title }}</h2>
     
-    <p>By. <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     
     {!! $post->body !!}
 
